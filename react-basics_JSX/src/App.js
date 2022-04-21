b3d3292c808aad54157ff75014b8472526cbd854
MkMonir <khanmonir1335@gmail.com>
2022-04-21 21:35:30 +0600

    Dynamic values and conditionals in JSX

--- react-basics_JSX/src/App.js
+++ react-basics_JSX/src/App.js
@@ -1,7 +1,26 @@
 function App() {
+  const showComments = true;
+  const comments = [
+    { id: 1, text: 'Comment 1' },
+    { id: 2, text: 'Comment 2' },
+    { id: 3, text: 'Comment 3' },
+  ];
+
+  const commentsBlock = (
+    <div className="comments">
+      <h3>Comments ({comments.length})</h3>
+      <ul>
+        {comments.map((comment, index) => (
+          <li key={index}>{comment.text}</li>
+        ))}
+      </ul>
+    </div>
+  );
+
   return (
     <div className="container">
       <h1>My App</h1>
+      {showComments && commentsBlock}
     </div>
   );
 }
