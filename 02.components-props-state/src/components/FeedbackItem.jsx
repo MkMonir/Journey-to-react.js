c63f5518a06c4211f55da42e1a9c970a8842ecd3
MkMonir <khanmonir1335@gmail.com>
2022-04-23 15:06:31 +0600

    Managing global state

--- 02.components-props-state/src/components/FeedbackItem.jsx
+++ 02.components-props-state/src/components/FeedbackItem.jsx
@@ -1,20 +1,8 @@
-import { useState } from 'react';
-
-function FeedbackItem() {
-  const [rating, setRating] = useState(8);
-  const [text, setText] = useState('This is a example of a feedback item');
-
-  const handleClick = (e) => {
-    setRating((prev) => {
-      return prev + 1;
-    });
-  };
-
+function FeedbackItem({ item }) {
   return (
     <div className="card">
-      <div className="num-display">{rating}</div>
-      <div className="text-display">{text}</div>
-      <button onClick={handleClick}>Click</button>
+      <div className="num-display">{item.rating}</div>
+      <div className="text-display">{item.text}</div>
     </div>
   );
 }
