6100ab2deef1ece9266a85a2348926caa5e32f4d
MkMonir <khanmonir1335@gmail.com>
2022-04-25 01:42:03 +0600

    Implementing real time validation in from input

--- 03.form-validation-and-simple-animations/src/components/FeedbackForm.jsx
+++ 03.form-validation-and-simple-animations/src/components/FeedbackForm.jsx
@@ -1,10 +1,24 @@
 import { useState } from 'react';
 import Card from './../shared/Card';
+import Button from './../shared/Button';
 
 function FeedbackForm() {
   const [text, setText] = useState('');
+  const [btnDisabled, setBtnDisabled] = useState(true);
+  const [message, setMessage] = useState('');
 
   const handleTextChange = (e) => {
+    if (text === '') {
+      setBtnDisabled(true);
+      setMessage(null);
+    } else if (text !== '' && text.trim().length <= 10) {
+      setBtnDisabled(true);
+      setMessage('Text must be at least 10 characters');
+    } else {
+      setBtnDisabled(false);
+      setMessage(null);
+    }
+
     setText(e.target.value);
   };
 
@@ -20,8 +34,12 @@ function FeedbackForm() {
             placeholder="Write a review"
             value={text}
           />
-          <button type="submit">Send</button>
+          <Button type="submit" isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+
+        {message && <div className="message">{message}</div>}
       </form>
     </Card>
   );
