42bf0f6bbe6e56347e8409bc1195d7e183f6f8b2
MkMonir <khanmonir1335@gmail.com>
2022-04-24 15:40:25 +0600

    Form input

---
+++ 03.form-validation-and-simple-animations/src/App.js
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+import Header from './components/Header';
+import FeedbackList from './components/FeedbackList';
+import FeedbackStats from './components/FeedbackStats';
+import FeedbackForm from './components/FeedbackForm';
+import FeedbackData from './data/FeedbackData';
+
+function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+
+  const deleteFeedback = (id) => {
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackForm />
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+      </div>
+    </>
+  );
+}
+
+export default App;
