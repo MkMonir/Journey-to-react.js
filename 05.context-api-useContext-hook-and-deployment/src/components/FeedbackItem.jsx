01f38afbf7fe35be3868466184b2155ef7e729b8
MkMonir <khanmonir1335@gmail.com>
2022-04-29 02:06:36 +0600

    Implementing edit feedback event

--- 05.context-api-useContext-hook-and-deployment/src/components/FeedbackItem.jsx
+++ 05.context-api-useContext-hook-and-deployment/src/components/FeedbackItem.jsx
@@ -1,17 +1,20 @@
-import { FaTimes } from 'react-icons/fa';
+import { FaTimes, FaEdit } from 'react-icons/fa';
 import { useContext } from 'react';
 import PropTypes from 'prop-types';
 import Card from './shared/Card';
 import FeddbackContext from './../context/FeedbackContext';
 
 function FeedbackItem({ item }) {
-  const { deleteFeedback } = useContext(FeddbackContext);
+  const { deleteFeedback, editFeedback } = useContext(FeddbackContext);
 
   return (
     <Card>
       <div className="num-display">{item.rating}</div>
       <button onClick={() => deleteFeedback(item.id)} className="close">
         <FaTimes color="purple" />
+      </button>
+      <button className="edit" onClick={() => editFeedback(item)}>
+        <FaEdit color="purple" />
       </button>
       <div className="text-display">{item.text}</div>
     </Card>
