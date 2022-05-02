5e825e174a4c2d79f1c7a50edf41a05d7e571d7e
MkMonir <khanmonir1335@gmail.com>
2022-05-03 00:53:44 +0600

    update and delete feedback from JSON-server

--- 06.api's-and-HTTP-requests/src/context/FeedbackContext.js
+++ 06.api's-and-HTTP-requests/src/context/FeedbackContext.js
@@ -36,15 +36,24 @@ export const FeedbackProvider = ({ children }) => {
   };
 
   // Delete Feedback
-  const deleteFeedback = (id) => {
+  const deleteFeedback = async (id) => {
     if (window.confirm('Are you sure you want to delete?')) {
+      await fetch(`/feedback/${id}`, { method: 'DELETE' });
       setFeedback(feedback.filter((item) => item.id !== id));
     }
   };
 
   // Update feedback
-  const updateFeedback = (id, updItem) => {
-    setFeedback(feedback.map((item) => (item.id === id ? { ...item, ...updItem } : item)));
+  const updateFeedback = async (id, updItem) => {
+    const res = await fetch(`/feedback/${id}`, {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(updItem),
+    });
+
+    const data = await res.json();
+
+    setFeedback(feedback.map((item) => (item.id === id ? { ...item, ...data } : item)));
 
     setFeedbackEdit({
       item: {},
