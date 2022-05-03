00cfaea618176f09b0d6f9c7009e330f38baaf0f
MkMonir <khanmonir1335@gmail.com>
2022-05-03 22:55:49 +0600

    create navbar component

---
+++ 07.github-finder-project/src/App.js
@@ -0,0 +1,15 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/layouts/Navbar';
+
+function App() {
+  return (
+    <Router>
+      <div className="flex flex-col justify-between h-screen ">
+        <Navbar />
+        <main>Content</main>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
