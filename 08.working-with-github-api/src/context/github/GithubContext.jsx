77c2cd36a5bb34bb2845f7fc2390a4cdf7c5b94a
MkMonir <khanmonir1335@gmail.com>
2022-05-16 23:58:00 +0600

    Cleaning up fetch users using reducer

--- 08.working-with-github-api/src/context/github/GithubContext.jsx
+++ 08.working-with-github-api/src/context/github/GithubContext.jsx
@@ -8,12 +8,15 @@ const GITHUB_API_URL = process.env.REACT_APP_GITHUB_API_URL;
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
-    loggedIn: true,
+    loading: false,
   };
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
+  // Get initial users (testing purposes)
   const fetchUsers = async () => {
+    setLoading();
+
     const res = await fetch(`${GITHUB_API_URL}/users`, {
       headers: { 'Content-Type': 'application/json' },
     });
@@ -25,6 +28,8 @@ export const GithubProvider = ({ children }) => {
       payload: data,
     });
   };
+
+  const setLoading = () => dispatch({ type: 'SET_LOADING' });
 
   return (
     <GithubContext.Provider value={{ users: state.users, loading: state.loading, fetchUsers }}>
