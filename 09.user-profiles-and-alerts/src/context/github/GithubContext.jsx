63de4ab4bde09d5c87f67ea6b7a3c485e8252e0e
MkMonir <khanmonir1335@gmail.com>
2022-05-22 17:04:23 +0600

    implementing github repos

--- 09.user-profiles-and-alerts/src/context/github/GithubContext.jsx
+++ 09.user-profiles-and-alerts/src/context/github/GithubContext.jsx
@@ -3,72 +3,21 @@ import githubReducer from './GithubReducer';
 
 const GithubContext = createContext();
 
-const GITHUB_API_URL = process.env.REACT_APP_GITHUB_API_URL;
-
 export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
   };
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  // Get Search Users
-  const searchUsers = async (text) => {
-    setLoading();
-
-    const params = new URLSearchParams({
-      q: text,
-    });
-
-    const res = await fetch(`${GITHUB_API_URL}/search/users?${params}`, {
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    const { items } = await res.json();
-
-    dispatch({
-      type: 'GET_USERS',
-      payload: items,
-    });
-  };
-
-  // Get Single User
-  const getUser = async (login) => {
-    setLoading();
-
-    const res = await fetch(`${GITHUB_API_URL}/users/${login}`, {
-      headers: { 'Content-Type': 'application/json' },
-    });
-
-    if (res.status === 404) {
-      window.location = '/notfound';
-    } else {
-      const data = await res.json();
-
-      dispatch({
-        type: 'GET_USER',
-        payload: data,
-      });
-    }
-  };
-
-  // Clear users from state
-  const clearUsers = () => dispatch({ type: 'CLEAR_USERS' });
-
-  // Set Loading
-  const setLoading = () => dispatch({ type: 'SET_LOADING' });
-
   return (
     <GithubContext.Provider
       value={{
-        users: state.users,
-        user: state.user,
-        loading: state.loading,
-        searchUsers,
-        clearUsers,
-        getUser,
+        ...state,
+        dispatch,
       }}
     >
       {children}
