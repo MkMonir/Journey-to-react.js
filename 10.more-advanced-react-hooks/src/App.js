4681573fe6f95bb8e3b67bcb6bce3695ec997f9f
MkMonir <khanmonir1335@gmail.com>
2022-05-25 01:45:37 +0600

    get previous state value using useRef hook

--- 10.more-advanced-react-hooks/src/App.js
+++ 10.more-advanced-react-hooks/src/App.js
@@ -1,10 +1,10 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import UseRefExample1 from './components/UseRefExample1';
+import UseRefExample2 from './components/UseRefExample2';
 
 function App() {
   return (
     <div className="container mt-5">
-      <UseRefExample1 />
+      <UseRefExample2 />
     </div>
   );
 }
