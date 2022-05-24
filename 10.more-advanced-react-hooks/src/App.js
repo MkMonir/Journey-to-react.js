4c4bbae7ec3a96f544f76a02488cd4fc88a70886
MkMonir <khanmonir1335@gmail.com>
2022-05-24 13:52:25 +0600

    more advanced react hooks

---
+++ 10.more-advanced-react-hooks/src/App.js
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="container mt-5">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
