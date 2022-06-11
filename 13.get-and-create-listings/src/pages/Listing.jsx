c0747b13658798e38769d96f45259ac8ff40ec62
MkMonir <khanmonir1335@gmail.com>
2022-06-12 02:11:30 +0600

    fetch single listing from firebase

---
+++ 13.get-and-create-listings/src/pages/Listing.jsx
@@ -0,0 +1,35 @@
+import { useState, useEffect } from 'react';
+import { useNavigate, useParams, Link } from 'react-router-dom';
+import { doc, getDoc } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { db } from '../firebase.config';
+import Spinner from '../components/Spinner';
+import shareIcon from '../assets/svg/shareIcon.svg';
+
+const Listing = () => {
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [shareLinkCopied, setShareLinkCopied] = useState(false);
+
+  const navigate = useNavigate();
+  const params = useParams();
+  const auth = getAuth();
+
+  useEffect(() => {
+    const fetchListing = async () => {
+      const docRef = doc(db, 'listings', params.listingId);
+      const docSnap = await getDoc(docRef);
+
+      if (docSnap.exists()) {
+        console.log(docSnap.data());
+        setListing(docSnap.data());
+        setLoading(false);
+      }
+    };
+
+    fetchListing();
+  }, [navigate, params.listingId]);
+
+  return <div>Listing</div>;
+};
+export default Listing;
