ab9cfb0af8f97d2d18f93289e3f65db88db3d12b
MkMonir <khanmonir1335@gmail.com>
2022-06-12 02:12:15 +0600

    add listing and create listing routes

--- 13.get-and-create-listings/src/App.js
+++ 13.get-and-create-listings/src/App.js
@@ -8,6 +8,8 @@ import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
+import CreateListing from './pages/CreateListing';
+import Listing from './pages/Listing';
 import Navbar from './components/Navbar';
 import PrivateRoute from './components/PrivateRoute';
 
@@ -25,6 +27,8 @@ function App() {
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="/category/:categoryName/:listingId" element={<Listing />} />
         </Routes>
         <Navbar />
       </Router>
