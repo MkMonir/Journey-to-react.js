abd7916aab4797480a5cd570c78590fc42a6a342
MkMonir <khanmonir1335@gmail.com>
2022-06-11 17:20:17 +0600

    uploading images to firebase

--- 13.get-and-create-listings/src/pages/CreateListing.jsx
+++ 13.get-and-create-listings/src/pages/CreateListing.jsx
@@ -1,8 +1,10 @@
 import { useState, useEffect, useRef } from 'react';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 import { db } from '../firebase.config';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
+import { v4 as uuidv4 } from 'uuid';
 import Spinner from '../components/Spinner';
 
 const CreateListing = () => {
@@ -61,7 +63,7 @@ const CreateListing = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isMounted]);
 
-  const onSubmit = (e) => {
+  const onSubmit = async (e) => {
     e.preventDefault();
 
     setLoading(true);
@@ -82,6 +84,11 @@ const CreateListing = () => {
     let location;
 
     if (geolocationEnabled) {
+      const data = {
+        geoLat: 23.7512562,
+        geoLng: 90.3861979,
+        geoLoc: '3 No Tejturi Bazar West, Panthapath, Dhaka 1215',
+      };
       geolocation.lat = data.geoLat;
       geolocation.lng = data.geoLng;
       location = data.geoLoc;
@@ -96,6 +103,51 @@ const CreateListing = () => {
       geolocation.lng = longitude;
       location = address;
     }
+
+    // Store Image in firebase
+    const storeImage = async (image) => {
+      return new Promise((resolve, reject) => {
+        const storage = getStorage();
+        const fileName = `${auth.currentUser.uid}-${image.name}-${uuidv4()}`;
+
+        const storageRef = ref(storage, 'images/' + fileName);
+
+        const uploadTask = uploadBytesResumable(storageRef, image);
+
+        uploadTask.on(
+          'state_changed',
+          (snapshot) => {
+            const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            console.log('Upload is ' + progress + '% done');
+            switch (snapshot.state) {
+              case 'paused':
+                console.log('Upload is paused');
+                break;
+              case 'running':
+                console.log('Upload is running');
+                break;
+              default:
+                break;
+            }
+          },
+          (error) => {
+            reject(error);
+          },
+          () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+              resolve(downloadURL);
+            });
+          }
+        );
+      });
+    };
+
+    const imgUrls = await Promise.all([...images].map((image) => storeImage(image))).catch(() => {
+      setLoading(false);
+      toast.error('Image not uploaded');
+      return;
+    });
+    console.log(imgUrls);
 
     setLoading(false);
   };
